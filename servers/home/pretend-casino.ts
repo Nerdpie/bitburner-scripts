df6b4d28e44f701f542cefea040ddf6b96cab095
Nerdpie <Nerdpie@users.noreply.github.com>
2024-12-21 12:32:31 -0500

    Make pretend-casino.ts actually check for travel money

--- servers/home/pretend-casino.ts
+++ servers/home/pretend-casino.ts
@@ -1,4 +1,4 @@
-import { exposeGameInternalObjects } from './scripts/lib/exploits'
+import {exposeGameInternalObjects} from './scripts/lib/exploits'
 
 export async function main(ns: NS) {
   if (!globalThis.Player) {
@@ -8,13 +8,20 @@ export async function main(ns: NS) {
   // noinspection ConfusingFloatingPointLiteralJS - 10 billion
   const MAX_CASINO_MONEY = 10e9;
 
+// noinspection ConfusingFloatingPointLiteralJS - 200 thousand
+  const TRAVEL_COST = 200e3;
+
   const casinoMoney = ns.getMoneySources().sinceInstall.casino;
-  if ( casinoMoney >= MAX_CASINO_MONEY ) {
+  if (casinoMoney >= MAX_CASINO_MONEY) {
     return;
   }
 
   if (globalThis.Player.city !== 'Aevum') {
+    if (globalThis.Player.money < TRAVEL_COST) {
+      ns.tprint('ERROR: Insufficient funds to travel!');
+      return;
+    }
     globalThis.Player.travel('Aevum');
   }
-  globalThis.Player.gainMoney(MAX_CASINO_MONEY - casinoMoney,'Casino');
+  globalThis.Player.gainMoney(MAX_CASINO_MONEY - casinoMoney, 'Casino');
 }
